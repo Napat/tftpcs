eb00fbbafea2eb3bca93b1aafbc09926b7937838
Napat Rungruangbangchan <napat@forth.co.th>
2017-06-12 17:34:02 +0700

    Decorate code by Lindent

--- tftp.h
+++ tftp.h
@@ -1,7 +1,7 @@
 /**********************************************************
 Date: 		OCT 28, 2006
 Project :	TFTP Server
-Programer:	Reza Rahmanian, Craig Holmes
+Programer:	Reza Rahmanian, Craig Holmes, Napat Rungruangbangchan
 File:		Header Files
 Purpose:	This file contains all the needed header files 
 		for the implementation of the TFTP client and servers.
@@ -9,35 +9,33 @@ Purpose:	This file contains all the needed header files
 
 #include <stdio.h>		/*for input and output */
 #include <string.h>		/*for string functions */
-#include <stdlib.h> 		/**/
-#include <unistd.h> 		/**/
+#include <stdlib.h> /**/
+#include <unistd.h> /**/
 #include <sys/types.h>		/*used for the socket function */
 #include <sys/socket.h>		/*for socket functions */
 #include <netdb.h>		/*for address information and conversions, also holds the hostent structure */
 #include <netinet/in.h>		/*for internet addresses */
-#include <arpa/inet.h> 		/**/
+#include <arpa/inet.h> /**/
 #include <errno.h>
 #include <sys/wait.h>
 #include <dirent.h>
-
 #define ERROR -1
-
 #define RRQ 1
 #define WRQ 2
 #define DATA 3
 #define ACK 0x04
 #define ERR 0x05
-
-#define TIMEOUT 2000 /*amount of time to wait for an ACK/Data Packet in 1000microseconds 1000 = 1 second*/
-#define RETRIES 3 /* Number of times to resend a data OR ack packet beforing giving up */
-#define MAXACKFREQ 16 /* Maximum number of packets before ack */
-#define MAXDATASIZE 1024 /* Maximum data size allowed */
+#define TIMEOUT 2000		/*amount of time to wait for an ACK/Data Packet in 1000microseconds 1000 = 1 second */
+#define RETRIES 3		/* Number of times to resend a data OR ack packet beforing giving up */
+#define MAXACKFREQ 16		/* Maximum number of packets before ack */
+#define MAXDATASIZE 1024	/* Maximum data size allowed */
 static char buf[BUFSIZ];	/*a buffer for the messages passed between the client and the server */
 
-char err_msg [7][40] = {"Not defined, see error message if any",
-                        "File not fount",
-                        "Access Violation",
-                        "Disk full, or allocation exceeded",
-                        "Illegal TFTP operation",
-                        "Unknown transfer ID",
-                        "File already exists"};
+char err_msg[7][40] = { "Not defined, see error message if any",
+	"File not fount",
+	"Access Violation",
+	"Disk full, or allocation exceeded",
+	"Illegal TFTP operation",
+	"Unknown transfer ID",
+	"File already exists"
+};
